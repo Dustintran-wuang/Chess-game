d4cd34c0bd924a5e62ff672cd3a961c75a5b4328
Team 2 <dustintran121206@gmail.com>
2025-06-24 17:51:14 +0700

    modify some headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Test Project");
+    window.setFramerateLimit(60); 
+
+
+    sf::CircleShape shape(100.f); 
+    shape.setFillColor(sf::Color::Green); 
+    
+    shape.setOrigin(shape.getRadius(), shape.getRadius());
+    shape.setPosition(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        window.draw(shape);
+
+        window.display();
+    }
+
     return 0;
 }
